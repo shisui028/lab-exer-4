2d806c4d032d4742fab349327309fca594371cdf
aelbren angeles <46342280+shisui028@users.noreply.github.com>
2019-01-17 17:06:09 +0800

    Update 03-angeles.cpp

--- 03-angeles.cpp
+++ 03-angeles.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
-	int num[5]={1,2,3,4,5};
+	int num[5];
 	int* ptr=num;
-
-	for(int x=0;x<5;x++)
+	
+	cout<<"Enter 5 numbers: "<<endl;
+	cin>>num[0]>>num[1]>>num[2]>>num[3]>>num[4];
+	cout<<"Your inputs are: "<<endl;
+	for(int x=1;x<=5;x++)
 	{
 	cout<<*ptr<<endl;
 	ptr++;
